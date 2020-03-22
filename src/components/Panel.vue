0391fdabba88301c41833593abd71939f0ce33d2
Darana Vidya <darana.sv@gmail.com>
2020-03-22 13:40:52 +0700

    First Commit

---
+++ src/components/Panel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="col" class="panel">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Panel'
+}
+</script>
+
+<style lang="scss">
+  .panel {
+    h1 {
+      margin: 0;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Panel />
+    <Panel />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Panel from '@/components/Panel'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Panel
   }
 }
 </script>
