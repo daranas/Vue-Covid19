047eb77f1596e0d5db4aec7a0a1f697195b82a70
Darana Vidya <darana.sv@gmail.com>
2020-03-24 00:02:54 +0700

    remove unused code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Map />
     <Panel />
     <Footer />
@@ -8,8 +7,6 @@
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
 import Panel from '@/components/Panel'
 import Map from '@/components/Map'
 import Footer from '@/components/Footer'
@@ -17,7 +14,6 @@ import Footer from '@/components/Footer'
 export default {
   name: 'Home',
   components: {
-    // HelloWorld,
     Panel,
     Map,
     Footer
